4b3f53b67680bd4fc0d13b1a9ddf39d645c3b209
onezeun <onezeun@gmail.com>
2022-11-07 16:36:31 +0900

    1107 글쓰기, 글 목록 조회

--- database/web_project/admin/index.php
+++ database/web_project/admin/index.php
@@ -20,7 +20,6 @@ if($s_id != "admin"){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>관리자 페이지</title>
     <style>
-        body{font-size:20px}
         a{text-decoration:none;margin:0 5px}
     </style>
 </head>
@@ -37,7 +36,7 @@ if($s_id != "admin"){
     <div class="nav">
         <a href="./">[홈으로]</a>
         <a href="members/list.php">[회원관리]</a>
-        <a href="notice/list.php">[공지사항]</a>
+        <a href="../notice/list.php">[공지사항]</a>
         <a href="event/list.php">[이벤트]</a>
         <a href="products/list.php">[상품관리]</a>
     </div>
